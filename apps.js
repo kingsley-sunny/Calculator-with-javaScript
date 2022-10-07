a5f0e8cafffe61b903fd49ef76d44930b039a4de
kingsley-sunny <ezeobisunny51@gmail.com>
2022-10-07 16:28:28 +0100

    made the calculator realtime

--- apps.js
+++ apps.js
@@ -6,41 +6,57 @@ const btns = document.querySelectorAll(".clickToShowBtn");
 const delBtn = document.querySelector(".delBtn");
 const clearBtn = document.querySelector(".clearBtn");
 
-
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    
-    console.log(resultShown.textContent)
-    if(typeof parseFloat(resultShown.textContent) === 'number'){
+
+    console.log(resultShown.textContent);
+    if (typeof parseFloat(resultShown.textContent) === "number") {
         resultShown.textContent = Function("return " + input.value)();
     } else {
-        resultShown.textContent = 'Error'
+        resultShown.textContent = "Error";
     }
 
     // resultShown.textContent = Function("return " + input.value)();
-  
 });
 
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    input.value += btn.textContent;
-  });
+    btn.addEventListener("click", function () {
+        input.value += btn.textContent;
+        if (typeof parseFloat(resultShown.textContent) === "number") {
+            resultShown.textContent = Function("return " + input.value)();
+        } else {
+            resultShown.textContent = "Error";
+        }
+    });
 });
 
 delBtn.addEventListener("click", function () {
-  input.value = input.value.substr(0, input.value.length - 1);
-  // console.log(input.value.substr(0, 5))
+    input.value = input.value.substr(0, input.value.length - 1);
+
+    if (typeof parseFloat(resultShown.textContent) === "number") {
+        if (input.value === "") {
+            resultShown.textContent = 0;
+        } else {
+            resultShown.textContent = Function("return " + input.value)();
+        }
+    } else {
+        resultShown.textContent = "Error";
+    }
 });
 
 clearBtn.addEventListener("click", function () {
-  input.value = ''
+    input.value = "";
+    resultShown.textContent = 0;
 });
 
-equalToBtn.addEventListener('click', function(){
-    if(typeof parseFloat(resultShown.textContent) === 'number'){
-        resultShown.textContent = Function("return " + input.value)();
+equalToBtn.addEventListener("click", function () {
+    if (typeof parseFloat(resultShown.textContent) === "number") {
+        if (input.value === "") {
+            resultShown.textContent = 0;
+        } else {
+            resultShown.textContent = Function("return " + input.value)();
+        }
     } else {
-        resultShown.textContent = 'Error'
+        resultShown.textContent = "Error";
     }
-})
-
+});
